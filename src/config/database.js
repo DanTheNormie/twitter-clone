1674a4298a585948eb129665260e900b943d3b86
CmdDaniel <cmd.princedaniel@gmail.com>
2023-07-17 12:10:35 +0530

    some more prod changes

--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 require('dotenv').config()
 
-const db_url = process.env.PROD ? process.env.DB_URL : process.env.DB_URL_PROD;
+const db_url = process.env.PROD ? process.env.DB_URL_PROD : process.env.DB_URL;
 
 function connect_db(){
     mongoose.connect(db_url,{useNewUrlParser: true, useUnifiedTopology: true})
-    .then(()=>{console.log('connection made successfully');})
+    .then(()=>{console.log(`connection made successfully at ${db_url}`);})
     .catch((err)=>{console.log(`connection to DB failed,\n Reason : ${err}`);})
 }
 
